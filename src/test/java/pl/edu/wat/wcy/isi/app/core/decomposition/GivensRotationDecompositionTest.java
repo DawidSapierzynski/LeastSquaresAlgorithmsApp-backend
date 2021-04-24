874c282892dc2f9453eba0b0210ced40fe7da6f1
Dawid Sapierzyński <dawid.sapierzynski@student.wat.edu.pl>
2021-04-24 11:20:45 +0200

    Updating the GivensRotationDecompositionTest

--- src/test/java/pl/edu/wat/wcy/isi/app/core/decomposition/GivensRotationDecompositionTest.java
+++ src/test/java/pl/edu/wat/wcy/isi/app/core/decomposition/GivensRotationDecompositionTest.java
@@ -9,8 +9,9 @@ import java.util.Random;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GivensRotationDecompositionTest {
-    public static final int N = 4;
-    public static final double DELTA = 0.000000001;
+    public static final int N = 10;
+    public static final double DELTA = 1e-10;
+    public static final double G = 10000;
     private Matrix m;
     private Random random;
 
@@ -21,10 +22,10 @@ class GivensRotationDecompositionTest {
     }
 
     private double[][] getRandomArray() {
-        double[][] array = new double[N][N];
-        for (int i = 0; i < N; i++) {
+        double[][] array = new double[N * 2][N];
+        for (int i = 0; i < N * 2; i++) {
             for (int j = 0; j < N; j++) {
-                array[i][j] = this.random.nextDouble();
+                array[i][j] = this.random.nextDouble() * G - G / 2;
             }
         }
         return array;
@@ -35,10 +36,39 @@ class GivensRotationDecompositionTest {
         GivensRotationDecomposition givensRotationDecomposition = new GivensRotationDecomposition(this.m);
         givensRotationDecomposition.decompose();
         Matrix matrixR = givensRotationDecomposition.R;
+        Matrix matrixQ = givensRotationDecomposition.Q;
 
+        upperTriangularMatrixTest(matrixR);
+        orthogonalMatrixTest(matrixQ);
+        matrixCompatibilityTest(matrixR, matrixQ);
+    }
+
+    void upperTriangularMatrixTest(Matrix matrixR) {
         for (int i = 0; i < matrixR.getColumnDimension(); i++) {
             for (int j = matrixR.getRowDimension() - 1; j > i; j--) {
                 assertEquals(0.0, matrixR.get(j, i), DELTA);
+            }
+        }
+    }
+
+    void orthogonalMatrixTest(Matrix matrixQ) {
+        int n = matrixQ.getRowDimension();
+        Matrix indetity = Matrix.identity(n, n);
+        Matrix q = matrixQ.times(matrixQ.transpose());
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                assertEquals(indetity.get(i, j), q.get(i, j), DELTA);
+            }
+        }
+    }
+
+    void matrixCompatibilityTest(Matrix matrixR, Matrix matrixQ) {
+        Matrix matrixA = matrixQ.times(matrixR);
+
+        for (int i = 0; i < this.m.getRowDimension(); i++) {
+            for (int j = 0; j < this.m.getColumnDimension(); j++) {
+                assertEquals(this.m.get(i, j), matrixA.get(i, j), DELTA);
             }
         }
     }
